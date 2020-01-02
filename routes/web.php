824b7f5c4829da4f24b8e24fcaf3e56afbe139ca
Adebayo Olaonipekun <adebayool@armpension.com>
2020-01-02 18:40:44 +0100

    one to many

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,60 @@
 |
 */
 
+use App\User;
+use App\Post;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/createpost/{userid}', function($userid) {
+
+    $user = User::findOrFail($userid);
+
+    $post = new Post([
+        'title' => 'My Third Post with Adebayo',
+        'body' => 'Body of my third post'
+    ]);
+
+    $user->posts()->save($post);
+
+});
+
+Route::get('/read/{id}', function($id) {
+
+    $user = User::findOrFail($id);
+
+    foreach($user->posts as $post) {
+
+        echo '<strong>' . $post->title . '</strong> ===== ' . $post->body . '<br>';
+
+    }
+
+});
+
+Route::get('/update/{user_id}/{post_id}', function($user_id, $post_id) {
+
+    $user = User::find($user_id);
+
+    $user->posts()->whereId($post_id)->update(['title' => 'Updated title', 'body' => 'Updated ccontent!']);
+
+});
+
+Route::get('/delete/{user_id}/{post_id}', function($user_id, $post_id) {
+
+    $user = User::find($user_id);
+
+    $user->posts()->whereId($post_id)->delete();
+
+});
+
+// Reverse of OneToMany
+Route::get('/read2/{post_id}', function($post_id) {
+
+    $post = Post::find($post_id);
+
+    return $post->user->name;
+
 });
